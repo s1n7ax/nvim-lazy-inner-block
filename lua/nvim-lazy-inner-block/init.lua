6cc8eed831127aa5e60ad7f2f190e644e2bb7ed8
s1n7ax <srineshnisala@gmail.com>
2021-09-24 04:36:54 +0530

    feat: add no remap to keymaps

--- lua/nvim-lazy-inner-block/init.lua
+++ lua/nvim-lazy-inner-block/init.lua
@@ -3,6 +3,7 @@ local api = vim.api
 local function map_keys(bufnr)
     local keys = {'{', '}', '(', ')', '[', ']', '<', '>', "'", '"', '`'}
     local operators = { 'd', 'y', 'v', 'c' }
+    local keymap_opt = {noremap = true}
 
     for _, key in ipairs(keys) do
         for _, operator in ipairs(operators) do
@@ -11,9 +12,9 @@ local function map_keys(bufnr)
             local action = operator .. 'i' .. key
 
             if bufnr then
-                api.nvim_buf_set_keymap(bufnr, 'n', keymap, action, {})
+                api.nvim_buf_set_keymap(bufnr, 'n', keymap, action, keymap_opt)
             else
-                api.nvim_set_keymap('n', keymap, action, {})
+                api.nvim_set_keymap('n', keymap, action, keymap_opt)
             end
         end
     end
