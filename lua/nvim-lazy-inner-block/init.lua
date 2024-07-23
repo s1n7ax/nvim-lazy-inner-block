ff298bd8d69a6ba5d58728f78cfe7657b692f55d
s1n7ax <srineshnisala@gmail.com>
2024-07-24 00:14:30 +0530

    refactor: use operator pending to mappings to do remap

--- lua/nvim-lazy-inner-block/init.lua
+++ lua/nvim-lazy-inner-block/init.lua
@@ -1,28 +1,21 @@
-local api = vim.api
-
 local function map_keys(bufnr)
-    local keys = {'{', '}', '(', ')', '[', ']', '<', '>', "'", '"', '`'}
-    local operators = { 'd', 'y', 'v', 'c' }
-    local keymap_opt = {noremap = true}
+	local keys = { "{", "}", "(", ")", "[", "]", "<", ">", "'", '"', "`" }
+	local keymap_opt = { noremap = true }
 
-    for _, key in ipairs(keys) do
-        for _, operator in ipairs(operators) do
+	for _, key in ipairs(keys) do
+		local action = "i" .. key
 
-            local keymap = operator .. key
-            local action = operator .. 'i' .. key
-
-            if bufnr then
-                api.nvim_buf_set_keymap(bufnr, 'n', keymap, action, keymap_opt)
-            else
-                api.nvim_set_keymap('n', keymap, action, keymap_opt)
-            end
-        end
-    end
+		if bufnr then
+			vim.api.nvim_buf_set_keymap(bufnr, "o", key, action, keymap_opt)
+		else
+			vim.api.nvim_set_keymap("o", key, action, keymap_opt)
+		end
+	end
 end
 
 local function setup(opts)
-    opts = opts or {}
-    map_keys(opts.bufnr)
+	opts = opts or {}
+	map_keys(opts.bufnr)
 end
 
-return {setup = setup}
+return { setup = setup }
